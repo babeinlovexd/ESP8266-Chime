a1bb34e46422172b8d7ebecd3a700c0ee421671a
babeinlovexd <43302033+babeinlovexd@users.noreply.github.com>
2026-03-17 23:58:40 +0000

    Refactor esp8266_chime to implement dual-mode state machine and custom I2S streaming.
    - Split functionality into independent Chime and Alarm modes with distinct UI entities.
    - Implemented a robust C++ state machine (IDLE, PLAYING_CHIME, PLAYING_ALARM) with proper priority handling (Alarm overrides Chime).
    - Replaced the external `ESP8266Audio` library with a custom, non-blocking I2S WAV player directly accessing the ESP8266 Arduino core (`core_esp8266_i2s.h`). This resolves complex PlatformIO LDF compilation failures related to `SD` and `SDFS`.
    - Expanded the built-in PROGMEM sound library from 5 to 10 unique sounds.
    - Auto-generated Home Assistant entities via Python helpers while keeping user YAML configuration minimal.
    - Updated README.md documentation to reflect all new features and configurations.

--- components/esp8266_chime/__init__.py
+++ components/esp8266_chime/__init__.py
@@ -1,11 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
-from esphome.components import number, select, button
+from esphome.components import number, select, button, switch
 from esphome.const import CONF_ID
 
 CODEOWNERS = ["@esphome"]
-DEPENDENCIES = []
+DEPENDENCIES = ["number", "select", "button", "switch"]
 
 esp8266_chime_ns = cg.esphome_ns.namespace("esp8266_chime")
 Esp8266Chime = esp8266_chime_ns.class_("Esp8266Chime", cg.Component)
@@ -15,13 +15,10 @@ CONF_WS = "ws"
 CONF_DOUT = "dout"
 CONF_SD = "sd"
 
-from esphome.const import CONF_VOLUME, CONF_MODE
-
-CONF_PLAY_BUTTON = "play_button"
-
 from . import number as esp8266_chime_number
 from . import select as esp8266_chime_select
 from . import button as esp8266_chime_button
+from . import switch as esp8266_chime_switch
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -30,17 +27,23 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Required(CONF_WS): pins.internal_gpio_output_pin_schema,
         cv.Required(CONF_DOUT): pins.internal_gpio_output_pin_schema,
         cv.Required(CONF_SD): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_VOLUME, default={"name": "Chime Volume"}): esp8266_chime_number.CONFIG_SCHEMA,
-        cv.Optional(CONF_MODE, default={"name": "Chime Sound"}): esp8266_chime_select.CONFIG_SCHEMA,
-        cv.Optional(CONF_PLAY_BUTTON, default={"name": "Chime Play"}): esp8266_chime_button.CONFIG_SCHEMA,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
+from .number import CONFIG_SCHEMA as NUMBER_SCHEMA
+from .select import CONFIG_SCHEMA as SELECT_SCHEMA
+from .button import CONFIG_SCHEMA as BUTTON_SCHEMA
+from .switch import CONFIG_SCHEMA as SWITCH_SCHEMA
+
+# Merge schemas from sub-components to allow them at the root component level
+CONFIG_SCHEMA = CONFIG_SCHEMA.extend(NUMBER_SCHEMA)
+CONFIG_SCHEMA = CONFIG_SCHEMA.extend(SELECT_SCHEMA)
+CONFIG_SCHEMA = CONFIG_SCHEMA.extend(BUTTON_SCHEMA)
+CONFIG_SCHEMA = CONFIG_SCHEMA.extend(SWITCH_SCHEMA)
 
 MULTI_CONF = True
 
 async def to_code(config):
-    cg.add_library("earlephilhower/ESP8266Audio", "1.9.7")
 
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
@@ -57,17 +60,12 @@ async def to_code(config):
     sd_pin = await cg.gpio_pin_expression(config[CONF_SD])
     cg.add(var.set_sd_pin(sd_pin))
 
-    if CONF_VOLUME in config:
-        conf = config[CONF_VOLUME]
-        conf[esp8266_chime_number.CONF_ESP8266_CHIME_ID] = var
-        await esp8266_chime_number.to_code(conf)
+    # We need to manually set esp8266_chime_id to point to var because we
+    # merged the schemas and don't require the user to specify it.
+    config_copy = dict(config)
+    config_copy["esp8266_chime_id"] = config[CONF_ID]
 
-    if CONF_MODE in config:
-        conf = config[CONF_MODE]
-        conf[esp8266_chime_select.CONF_ESP8266_CHIME_ID] = var
-        await esp8266_chime_select.to_code(conf)
-
-    if CONF_PLAY_BUTTON in config:
-        conf = config[CONF_PLAY_BUTTON]
-        conf[esp8266_chime_button.CONF_ESP8266_CHIME_ID] = var
-        await esp8266_chime_button.to_code(conf)
+    await esp8266_chime_number.to_code(config_copy)
+    await esp8266_chime_select.to_code(config_copy)
+    await esp8266_chime_button.to_code(config_copy)
+    await esp8266_chime_switch.to_code(config_copy)
--- components/esp8266_chime/select.py
+++ components/esp8266_chime/select.py
@@ -7,20 +7,39 @@ from . import esp8266_chime_ns, Esp8266Chime
 
 CONF_ESP8266_CHIME_ID = "esp8266_chime_id"
 
-Esp8266ChimeSoundSelect = esp8266_chime_ns.class_("Esp8266ChimeSoundSelect", select.Select)
+CONF_CHIME_SOUND = "chime_sound"
+CONF_ALARM_SOUND = "alarm_sound"
 
-CONFIG_SCHEMA = select.select_schema(Esp8266ChimeSoundSelect).extend(
+Esp8266ChimeSoundSelect = esp8266_chime_ns.class_("Esp8266ChimeSoundSelect", select.Select, cg.Component)
+Esp8266AlarmSoundSelect = esp8266_chime_ns.class_("Esp8266AlarmSoundSelect", select.Select, cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_ESP8266_CHIME_ID): cv.use_id(Esp8266Chime),
+        cv.Optional(CONF_CHIME_SOUND, default={"name": "Chime Ton"}): select.select_schema(Esp8266ChimeSoundSelect).extend(cv.COMPONENT_SCHEMA),
+        cv.Optional(CONF_ALARM_SOUND, default={"name": "Alarm Ton"}): select.select_schema(Esp8266AlarmSoundSelect).extend(cv.COMPONENT_SCHEMA),
     }
-).extend(cv.COMPONENT_SCHEMA)
-
+)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await select.register_select(var, config, options=["1. Ding Dong", "2. Trill Alarm", "3. Sweep Sound", "4. Solid Beep", "5. G5 Chime"])
+    hub = await cg.get_variable(config[CONF_ESP8266_CHIME_ID])
+    options = [
+        "1. Ding Dong", "2. Trill Alarm", "3. Sweep Sound", "4. Solid Beep", "5. G5 Chime",
+        "6. Siren", "7. Doorbell", "8. Notification", "9. Error", "10. Success"
+    ]
 
-    hub = config[CONF_ESP8266_CHIME_ID]
-    cg.add(var.set_parent(hub))
-    cg.add(hub.set_sound_select(var))
+    if CONF_CHIME_SOUND in config:
+        conf = config[CONF_CHIME_SOUND]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
+        await select.register_select(var, conf, options=options)
+        cg.add(var.set_parent(hub))
+        cg.add(hub.set_chime_sound_select(var))
+
+    if CONF_ALARM_SOUND in config:
+        conf = config[CONF_ALARM_SOUND]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
+        await select.register_select(var, conf, options=options)
+        cg.add(var.set_parent(hub))
+        cg.add(hub.set_alarm_sound_select(var))
